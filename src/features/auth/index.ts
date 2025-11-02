e45fbf52073bb9d5bb3a7fde18f725697526230a
goddi1999 <godfredasefa@icloud.com>
2025-11-02 10:14:59 +0100

    nameed export

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,14 +1,23 @@
 // Auth Components
-export { default as LoginForm } from './components/LoginForm';
-export { default as SignUpForm } from './components/SignUpForm';
-export { default as ForgotPasswordForm } from './components/ForgotPasswordForm';
+export {default as LoginForm} from './components/LoginForm';
+export {default as SignUpForm} from './components/SignUpForm';
+export {default as ForgotPasswordForm} from './components/ForgotPasswordForm';
 
 // Auth API
 export * from './api/authApi';
 
 // Auth Hooks
-export { default as useAuth } from './hooks/useAuth';
+export {default as useAuth} from './hooks/useAuth';
 
 // Auth Types
-export * from './types/auth.types';
+export type {
+    User,
+    SignUpData,
+    LoginData,
+    AuthResponse,
+    AuthState,
+    ForgotPasswordData,
+    ResetPasswordData,
+    VerifyEmailData,
+} from './types/auth.types';
 
