08858342249012d8435789202107695730e0db30
goddi1999 <godfredasefa@icloud.com>
2025-11-09 22:59:19 +0100

    update so assets inlcuded in theat build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -12,4 +10,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  // Ensure assets are included in build
+  assetsInclude: ['**/*.jpeg', '**/*.jpg', '**/*.png'],
 });
