4dba5b5c8fdca2938e508131aee7e403d8e1e4c0
Willfryd <godfredasefa@icloud.com>
2026-01-29 19:33:32 +0100

    update supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,6 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-console.log('supabaseUrl :>> ', supabaseUrl)
-console.log('supabaseKey :>> ', supabaseKey)
 export const supabase = createClient(supabaseUrl, supabaseKey)
